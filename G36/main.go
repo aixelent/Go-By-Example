ac8ca6956792cd36b28c41df4b76aa42f306bc26
xhexe <boulderdash@outlook.com>
2018-11-03 23:55:53 +0100

    Added G36 process information

--- G36/main.go
+++ G36/main.go
@@ -1,1 +1,21 @@
-package G36
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+)
+
+func main() {
+	p := os.Getpid()
+	fmt.Printf("Process ID: %d", p)
+
+	proc := exec.Command("ps", "-p", strconv.Itoa(p), "-v")
+	outpur, err := proc.Output()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(outpur))
+}
