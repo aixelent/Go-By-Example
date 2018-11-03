939e94c73842390e89591918d51a34aec794b4a9
xhexe <boulderdash@outlook.com>
2018-11-03 23:54:18 +0100

    Added G35 - CRUD file

---
+++ G35/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+var filePath = "file.txt"
+
+func createFile() {
+	f, err := os.Create(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	fmt.Println("File created.")
+}
+
+func readFile() {
+	file, err := os.OpenFile(filePath, os.O_RDWR, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	f, err := ioutil.ReadFile(filePath)
+	s := string(f)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(s)
+}
+
+func updateFileContent() {
+	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	toFile, err := file.WriteString("\n\nThe new IDE extends the IntelliJ platform with coding assistance and tool integrations specific for the Go language.")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("String was added to file correctly. Added %d bytes.", toFile)
+}
+
+func deleteFile() {
+	err := os.Remove(filePath)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("File was deleted.", filePath)
+}
+
+func main() {
+	createFile()
+	updateFileContent()
+	readFile()
+	deleteFile()
+}
