88408417e2ffc874c0f68dbab083abf617a6a4bc
xhexe <boulderdash@outlook.com>
2018-11-06 00:25:39 +0100

    Added G44 - replacing few different words

--- G44/main.go
+++ G44/main.go
@@ -1,4 +1,4 @@
-package _1
+package main
 
 import (
 	"fmt"
@@ -8,5 +8,7 @@ import (
 const words string = "Save time while PyCharm takes care of the routine. Focus on the bigger things and embrace the keyboard-centric approach to get the most of PyCharm’s many productivity features."
 
 func main() {
-	word := strings.Repla
+	word := strings.NewReplacer("takes", "LoL", "most", "LoL", "PyCharm", "LoL")
+	output := word.Replace(words)
+	fmt.Println(output)
 }
