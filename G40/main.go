9ad1eaafc4c54ccb1375b8c27b1367a41d0a36c1
xhexe <boulderdash@outlook.com>
2018-11-05 22:35:45 +0100

    Added G40 - executing any terminal command

--- G40/main.go
+++ G40/main.go
@@ -1,1 +1,23 @@
-package G40
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	// df -h - shows disc usage
+	proc := exec.Command("df", "-h")
+	out := bytes.NewBuffer([]byte{})
+	proc.Stdout = out
+
+	err := proc.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	if proc.ProcessState.Success() {
+		fmt.Println("Process run successfully:", out.String())
+	}
+}
