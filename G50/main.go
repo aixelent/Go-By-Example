7152a7d1ba2db9bae586c46a529e791163fd219b
xhexe <boulderdash@outlook.com>
2018-11-08 16:16:10 +0100

    Added G50 - Random char generator

--- G50/main.go
+++ G50/main.go
@@ -1,1 +1,22 @@
-package G50
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func randString(n int) string {
+	bytes := make([]byte, n)
+	for i := 0; i < 20; i++ {
+		bytes[i] = byte(randInt(97, 122))
+	}
+	return string(bytes)
+}
+
+func randInt(min, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+func main() {
+	fmt.Println(randString(20))
+}
