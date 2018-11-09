ff71221f49041d5c0faaa2923d59d97dbaf9faeb
xhexe <boulderdash@outlook.com>
2018-11-10 00:13:18 +0100

    Added G45 - reading CSV file

--- G45/main.go
+++ G45/main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
-	"strings"
+	"os"
 )
 
-const words string = "Save time while PyCharm takes care of the routine. Focus on the bigger things and embrace the keyboard-centric approach to get the most of PyCharm’s many productivity features."
+const filePath = "file.csv"
+
+func readFile() {
+	f, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	reader := csv.NewReader(f)
+	reader.FieldsPerRecord = 2
+
+	for {
+		record, err := reader.Read()
+		if err != nil {
+			panic(err)
+			break
+		}
+		fmt.Println(record)
+	}
+}
 
 func main() {
-	word := strings.NewReplacer("takes", "LoL", "most", "LoL", "PyCharm", "LoL")
-	output := word.Replace(words)
-	fmt.Println(output)
+	readFile()
 }
