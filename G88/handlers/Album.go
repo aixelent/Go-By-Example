89a0b385bbcdd12c9cf0f2da0da5f42e5825d3c6
xhexe <boulderdash@outlook.com>
2019-04-09 22:09:55 +0200

    G88 - Query params Echo framework

--- G88/handlers/Album.go
+++ G88/handlers/Album.go
@@ -1,34 +1,10 @@
 package handlers
 
 import (
-	"Go-By-Example/G88/models"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-
 	"github.com/labstack/echo"
+	"net/http"
 )
-
-func AddAlbum(e echo.Context) error {
-	albums := models.Album{}
-
-	defer e.Request().Body.Close()
-	r, err := ioutil.ReadAll(e.Request().Body)
-	if err != nil {
-		log.Fatalf("Cannot read request body %s:", err)
-		return e.String(http.StatusInternalServerError, "")
-	}
-
-	err = json.Unmarshal(r, &albums)
-	if err != nil {
-		log.Fatalf("Failed unmarshal data: %s", err)
-		return e.String(http.StatusInternalServerError, "")
-	}
-	log.Printf("Album: %s", albums)
-	return e.String(http.StatusOK, "Album response - Done.")
-}
 
 func GetAlbum(e echo.Context) error {
 	albumName := e.QueryParam("name")
@@ -36,13 +12,10 @@ func GetAlbum(e echo.Context) error {
 
 	data := e.Param("data")
 
-	// http://localhost:8080/album/string?name=Bvdub&genre=Electronic
 	if data == "string" {
-		// http://localhost:8080/album?name=Bvdub&genre=Electronic
-		return e.String(http.StatusOK, fmt.Sprintf("Album Name: %s.\nGenre: %s", albumName, albumGenre))
+		return e.String(http.StatusOK, fmt.Sprintf("Album Name: %s\nGenre: %s", albumName, albumGenre))
 	}
 
-	// http://localhost:8080/album/json?name=Bvdub&genre=Electronic
 	if data == "json" {
 		return e.JSON(http.StatusOK, map[string]string{
 			"name":  albumName,
@@ -51,6 +24,6 @@ func GetAlbum(e echo.Context) error {
 	}
 
 	return e.JSON(http.StatusBadRequest, map[string]string{
-		"error:": "asdf",
+		"error:": "Bad data type - string or json only",
 	})
 }
--- G88/models/Artist.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Artist struct {
-	Name    string `json:"name"`
-	Nick    string `json:"nick"`
-	Aliases string `json:"aliases"`
-}
