c25c331be83102cadc76045bfc9ad7091494de2c
xhexe <boulderdash@outlook.com>
2019-04-24 22:03:08 +0200

    G91 - Custom ECHO middleware

--- G91/handlers/Middleware.go
+++ G91/handlers/Middleware.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+func CustomHeader(hf echo.HandlerFunc) echo.HandlerFunc {
+	return func(e echo.Context) error {
+		e.Response().Header().Set(echo.HeaderServer, "Bot")
+		return hf(e)
+	}
+}
+
 func MiddlewareMain(e echo.Context) error {
 	return e.String(http.StatusOK, "Response from MiddlewareMain()")
 }
--- G91/main.go
+++ G91/main.go
@@ -1,26 +1,15 @@
 package main
 
 import (
-	"Go-By-Example/G90/handlers"
+	"Go-By-Example/G91/handlers"
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	e.Use(handlers.CustomHeader)
 	g := e.Group("/middleware")
-
-	g.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-		Format:           `[${time_rfc3339}] ${method} ${status} ${host}${path} ${latency} ${latency_human}` + "\n",
-		CustomTimeFormat: "2006-01-02 15:04:05",
-	}))
-
-	g.Use(middleware.BasicAuth(func(userLogin, password string, e echo.Context) (bool, error) {
-		if userLogin == "admin" && password == "admin123" {
-			return true, nil
-		}
-		return false, nil
-	}))
 
 	g.GET("/main", handlers.MiddlewareMain)
 
